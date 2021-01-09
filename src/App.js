5974b648f2010b6e1379307a57ac98cdfd1b5d1c
r_new <robert.newman@alumni.ubc.ca>
2021-01-09 18:52:09 -0500

    Refactored the search bar so that it can do props

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 // import Button from 'react-bootstrap/Button'
 import { Button, Alert } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import SearchBar from './Components/searchbar';
+import SearchBar from './Components/nonconstsearchbar'
 
 
 function App() {
@@ -13,10 +13,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <Alert>This is a button</Alert>
-        <Button>Test Button</Button>
-
-        <SearchBar></SearchBar>
+        <Alert>Search a book to see how long it takes to read it!</Alert>
+        
+        
+        <SearchBar placeholder_string = "Type the name of the book here!"></SearchBar>
+        <span></span>
+        <Button>Search</Button>
       </header>
     </div>
   );
