28e84cffa17ff87ed3682388571c39a18021995c
r_new <robert.newman@alumni.ubc.ca>
2021-01-10 00:45:27 -0500

    Changed footer

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
     return (
-        <footer className="bg-gray-200 text center text-xs p-3"> &copy; Copyright 2021 Ariana Simp Squad  </footer>
+        <footer className="bg-gray-200 text center text-xs p-3"> &copy; Copyright 2021 Arjun Dhaliwal, Clara Kim, Kyla Purcell, Robert Newman  </footer>
     )
 }
 
