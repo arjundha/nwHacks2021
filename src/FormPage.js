c9c54701bda97f9218e8a3576d6a56a1bccc31a5
r_new <robert.newman@alumni.ubc.ca>
2021-01-09 22:51:15 -0500

    Made the form only accept positive nums. Rnds2 int

--- src/FormPage.js
+++ src/FormPage.js
@@ -4,16 +4,20 @@ import './readtime.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NameForm from './Components/NameForm';
-
+import Footer from './Components/footer'
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <container>
         <img src={logo} className="App-logo" alt="logo" />
-      <NameForm></NameForm>
+        </container>
+        <span></span>
+        <NameForm></NameForm>
       </header>
+      <Footer></Footer>
     </div>
   );
 }
