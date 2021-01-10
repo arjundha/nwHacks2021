183d29b97329a44990d0790d94434208340a3ec6
Arjun <59514930+arjundha@users.noreply.github.com>
2021-01-09 22:56:45 -0800

    started about us

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Home from "../src/Containers/Home";
 import TEST from "../src/Containers/TEST"
 import AddTime from "../src/Containers/AddTime"
+import AboutUs from "../src/Containers/AboutUs"
 
 export default function Routes() {
   return (
@@ -15,6 +16,9 @@ export default function Routes() {
       </Route>
       <Route exact path="/add">
         <AddTime />
+      </Route>
+      <Route exact path="/about">
+        <AboutUs />
       </Route>
     </Switch>
   );
